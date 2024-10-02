fa22acce02adfa0c31975cfb9e235a6d5268c545
han18 <han.fadah@gmail.com>
2024-10-02 11:04:30 -0400

    created the menu buttons

--- main.js
+++ main.js
@@ -21,4 +21,13 @@ topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
+// Part 3: Adding Menu Buttons
 
+// iterating through
+for (let i = 0; i < menuLinks.length; i++) {
+  const aMenu = document.createElement("a");
+  aMenu.setAttribute("herf", menuLinks[i].href);
+  aMenu.innerHTML = menuLinks[i].text;
+  topMenuEl.appendChild(aMenu);
+}
+console.log(menuLinks);
