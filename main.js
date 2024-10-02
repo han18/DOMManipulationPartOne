2f99739317315d8b4192dfe25888fa8f54ec0191
han18 <han.fadah@gmail.com>
2024-10-02 10:29:54 -0400

    connected main to the js file

---
+++ main.js
@@ -0,0 +1,9 @@
+// this is part one
+// Menu data structure
+let menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
