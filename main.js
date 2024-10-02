24a9bb5178a9f498a4bb5e306a58f5cff4fe8b7a
han18 <han.fadah@gmail.com>
2024-10-02 11:02:15 -0400

    connected the menu bar

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ let menuLinks = [
   { text: "account", href: "/account" },
 ];
 
+// part 1
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
+mainEl.classList.add("flex-ctr");
+
+/// part 2 Creating a Menu Bar
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+
